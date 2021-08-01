6888c121182d02dfe983e96ea10a160ebcca2c4c
nolastemgarden <nolastemgarden@gmail.com>
2021-08-01 17:25:44 -0500

    Separate NewGameButtong from the Dialog it opens

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,14 +3,12 @@ import {
     Link as RouterLink,
 } from "react-router-dom"
 
-
+// MY components
+import NewGameButton from "../navigation/NewGameButton";
 
 // MUI  components
-import { Typography, Container } from '@material-ui/core'
-import { Button } from '@material-ui/core'
-
-
-
+import Typography from '@material-ui/core/Typography';
+import { Container } from '@material-ui/core'
 
 export default function Landing() {
 
@@ -19,14 +17,17 @@ export default function Landing() {
             <Typography color="text.primary" variant="h1">
                 Welcome to the Landing Page
             </Typography>
-            <Button
+            <NewGameButton />
+
+            {/* <Button
                 variant='contained'
                 color="primary"
                 component={RouterLink}
                 to='/game-settings'
             >
                 New Game
-            </Button>
+            </Button> */}
         </Container>
     );
 }
+
