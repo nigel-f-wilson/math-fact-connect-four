481482f93bff6de00fd48dfd1c62ea189c4f9cd5
nolastemgarden <nolastemgarden@gmail.com>
2021-08-01 13:46:59 -0500

    Create Square.js

---
+++ src/components/Square.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    Link as RouterLink,
+} from "react-router-dom"
+
+
+
+// MUI  components
+import { Box } from '@material-ui/core'
+
+
+
+export default function Square() {
+
+    return (
+        <Box>
+
+        </Box>
+    );
+}
