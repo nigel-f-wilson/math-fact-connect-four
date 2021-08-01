e1d7d9f88cb3f0c1c87af17ecbe7476e318654b2
nolastemgarden <nolastemgarden@gmail.com>
2021-08-01 13:47:13 -0500

    Create PlayVsHuman.js

---
+++ src/pages/PlayVsHuman.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    Link as RouterLink,
+} from "react-router-dom"
+
+
+
+// MUI  components
+import { Typography, Container } from '@material-ui/core'
+
+
+
+export default function PlayVsHuman() {
+
+    return (
+        <Container maxWidth="md">
+            <Typography color="text.primary" variant="h1">
+                Welcome to the PlayVsHuman Page
+            </Typography>
+        </Container>
+    );
+}
