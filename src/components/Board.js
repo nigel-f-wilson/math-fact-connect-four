3141817c3cbd1d8ebf4ab49ced766c424e4dd28f
nolastemgarden <nolastemgarden@gmail.com>
2021-08-01 13:46:52 -0500

    Create Board.js

---
+++ src/components/Board.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    Link as RouterLink,
+} from "react-router-dom"
+
+
+
+// MUI  components
+import { Box } from '@material-ui/core'
+
+
+
+
+export default function Board() {
+
+    return (
+        <Box>
+
+        </Box>
+    );
+}
