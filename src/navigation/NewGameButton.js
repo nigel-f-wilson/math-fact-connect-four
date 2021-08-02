83d19d9d22738bac8c64765b362100e2afd95a80
nolastemgarden <nolastemgarden@gmail.com>
2021-08-02 14:40:19 -0500

    Extract NewGameButton component from landing page

--- src/navigation/NewGameButton.js
+++ src/navigation/NewGameButton.js
@@ -1,1 +1,40 @@
 // NewGAmeButton opens the SettingsDialog
+
+import React from 'react'
+
+
+// MY components
+import SettingsDialog from "../settings/SettingsDialog";
+
+// MUI components
+import Button from '@material-ui/core/Button';
+
+
+export default function NewGameButton() {
+    const [open, setOpen] = React.useState(false);
+    // const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value) => {
+        setOpen(false);
+        // setSelectedValue(value);
+    };
+
+    return (
+        <React.Fragment >
+            <Button variant="outlined" onClick={handleClickOpen}>
+                New Game
+            </Button>
+            <SettingsDialog
+                // selectedValue={selectedValue}
+                open={open}
+                onClose={handleClose}
+            />
+        </React.Fragment>
+    );
+}
+
+
