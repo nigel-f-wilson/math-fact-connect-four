ea282ee9f65c9d4cec4e39cf1badb17b893fd171
nolastemgarden <nolastemgarden@gmail.com>
2021-08-03 13:54:32 -0500

    Replace map made version with explicit <Step>s

--- src/settings/SettingsStepper.js
+++ src/settings/SettingsStepper.js
@@ -12,7 +12,7 @@ const steps = [
     {
         label: 'Play vs. Human or Bot?',
         description: '',
-        buttons: []
+        buttons: [<PlayVsHuman setPlayMode={setPlayMode} />, <PlayVsBot setPlayMode={setPlayMode} />,]
 
     },
     {
@@ -67,40 +67,32 @@ export default function SettingsStepper() {
     return (
         <Box sx={{ maxWidth: 400 }}>
             <Stepper activeStep={activeStep} orientation="vertical">
-                {steps.map((step, index) => (
-                    <Step key={step.label}>
-                        <StepLabel
-                            optional={
-                                index === 2 ? (
-                                    <Typography variant="caption">Last step</Typography>
-                                ) : null
-                            }
-                        >
-                            {step.label}
-                        </StepLabel>
-                        <StepContent>
-                            <Typography>{step.description}</Typography>
-                            <Box sx={{ mb: 2 }}>
-                                <div>
-                                    <Button
-                                        variant="contained"
-                                        onClick={handleNext}
-                                        sx={{ mt: 1, mr: 1 }}
-                                    >
-                                        {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                                    </Button>
-                                    <Button
-                                        disabled={index === 0}
-                                        onClick={handleBack}
-                                        sx={{ mt: 1, mr: 1 }}
-                                    >
-                                        Back
-                                    </Button>
-                                </div>
-                            </Box>
-                        </StepContent>
-                    </Step>
-                ))}
+                <Step key={step.label}>
+                    <StepLabel>
+                        <Typography variant="caption">Last step</Typography>
+                    </StepLabel>
+                    <StepContent>
+                        <Typography>{step.description}</Typography>
+                        <Box sx={{ mb: 2 }}>
+                            <div>
+                                <PlayVsHumanButton
+                                    variant="contained"
+                                    onClick={handleNext}
+                                    sx={{ mt: 1, mr: 1 }}
+                                >
+                                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                                </Button>
+                                <Button
+                                    disabled={index === 0}
+                                    onClick={handleBack}
+                                    sx={{ mt: 1, mr: 1 }}
+                                >
+                                    Back
+                                </Button>
+                            </div>
+                        </Box>
+                    </StepContent>
+                </Step>
             </Stepper>
             {activeStep === steps.length && (
                 <Paper square elevation={0} sx={{ p: 3 }}>
