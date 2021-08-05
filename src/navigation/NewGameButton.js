03df9949fdea149cfe5bb60da024cb40c6f571e8
nolastemgarden <nolastemgarden@gmail.com>
2021-08-04 22:20:01 -0500

    Recombine settingsDialog parts in one file
    Even though this file is getting long and has more growing to do it is organized well and is readily searchable.  Defining these components all in one file is saving a lot of prop drilling as pointed out in Opinionated React.

--- src/navigation/NewGameButton.js
+++ src/navigation/NewGameButton.js
@@ -3,37 +3,96 @@ import PropTypes from 'prop-types';
 
 
 // MY components
-import SettingsDialog from "../settings/SettingsDialog";
 
 // MUI components
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import Stepper from '@material-ui/core/Stepper';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+
+
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+// MUI icons
+import CloseIcon from '@material-ui/icons/Close';
 
 export default function NewGameButton() {
-    const [open, setOpen] = React.useState(false);
-    // const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+    const [dialogOpen, setDialogOpen] = React.useState(false);
+    
+    
+    // const [selectedValue, setSelectedValue] = React.useState({
+    //     opponent: "human",
+    //     difficulty: "hard"
+    // });
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = (value) => {
-        setOpen(false);
-        // setSelectedValue(value);
-    };
+    const handleOpenDialog = () => { setDialogOpen(true) }
+    const handleCloseDialog = () => { setDialogOpen(false) }
 
     return (
         <React.Fragment >
-            <Button variant="outlined" onClick={handleClickOpen}>
+            <Button variant="contained" onClick={handleOpenDialog}>
                 New Game
             </Button>
             <SettingsDialog
-                // selectedValue={selectedValue}
-                open={open}
-                onClose={handleClose}
+                sx={{ width: 600 }}
+                dialogOpen={dialogOpen}
+                onClose={handleCloseDialog}
             />
         </React.Fragment>
     );
 }
 
+// SettingsDialog.propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     open: PropTypes.bool.isRequired,
+//     selectedValue: PropTypes.object.isRequired,
+// }
+function SettingsDialog(props) {
+    const { onClose, selectedValue, dialogOpen } = props;
+    const theme = useTheme()
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    
+
+    const handleListItemClick = (value) => {
+        onClose(value);
+    };
+
+    return (
+        <Dialog
+            onClose={onClose}
+            open={dialogOpen}
+            fullScreen={fullScreen}
+        >
+            <CloseIcon
+                sx={{ margin: '1rem 1rem 0 auto' }}
+                onClick={() => onClose()}
+            />
+
+            <DialogTitle sx={{ padding: '1rem' }}  >
+                What kind of game do you want to play?
+            </DialogTitle>
+
+            <DialogContent>
+                <SettingsStepper />
+
+
+            </DialogContent>
+        </Dialog>
+    );
+}
 
