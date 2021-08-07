be48d6a7cd776f6fc51f3e344ecfe3d97d19f419
nolastemgarden <nolastemgarden@gmail.com>
2021-08-06 22:16:31 -0500

    Add constants to size Squares and Chips

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -8,13 +8,22 @@ import {
 // MUI  components
 import { Box } from '@material-ui/core'
 
+// Style & Layout Constants
+const squarePercentage = '14.287%'
+const chipSizeRelativeToSquare = '84%'
 
 
 
 export default function GameBoard() {
 
+            width: squarePercentage,
+            height: '100%',
     return (
         <Box sx={{ bgcolor: 'board.main', mt: 4, pt: '100%', width: '100%' }} >
+            height: squarePercentage,
+            width: '100%',
+            height: chipSizeRelativeToSquare,
+            width: chipSizeRelativeToSquare,
 
         
         </Box>
