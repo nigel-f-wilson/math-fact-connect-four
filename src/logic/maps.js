3fb06e58bf654f6da4938ec6e401a102630aad6f
nolastemgarden <nolastemgarden@gmail.com>
2021-08-10 22:03:03 -0500

    Copy in lineIdToSquareIdsMap

--- src/logic/maps.js
+++ src/logic/maps.js
@@ -16,3 +16,87 @@ const numberOfHorizontalLines = linesPerRow * squaresPerCol;
 const numberOfUpslashLines = linesPerCol * linesPerRow;
 const numberOfDownslashLines = linesPerCol * linesPerRow;
 
+export const lineIdToSquareIdsMap = () => {
+    let completeMap = new Map();
+    let partialMaps = [verticalLineMap(), horizontalLineMap(), upslashLineMap(), downslashLineMap()]
+
+    partialMaps.forEach(partialMap => {
+        partialMap.forEach((squareIdList, lineId) => {
+            completeMap.set(lineId, squareIdList);
+        });
+    })
+    // CONFUSING point: For now I have just silenced the following console.log because it runs everytime handleColumnClick is called.  This means the map is being recreated from scratch each turn of the game unnecesarily. Perhaps I could solve this by moving the entire ClassicGame() inside a wrapper component that is strictly for holding complex CONSTANTS that ClassGame uses but only needs to compute once such as the Maps.
+    console.log(`Generated the Maps between lineIds and squareIds. Size: ${completeMap.size} LineIds in the Map.`)
+    // completeMap.forEach(logMapElement);
+    // The following four HELPERS bear responsibility for corresponding the lists of squares to their correct final lineIds.  This is simple for vertical lines but requires consideartion of the startingId for the latter three. 
+    function verticalLineMap() {
+        let map = new Map();
+        // Vertical Lines are assigned Ids starting from Zero.
+        let currentLineId = 0;
+
+            if (isStartOfVerticalLine(squareId)) {
+                let first = squareId + 0;
+                let second = squareId + 1;
+                let third = squareId + 2;
+                let fourth = squareId + 3;
+                let squaresInLine = [first, second, third, fourth];
+                map.set(currentLineId, squaresInLine);
+                currentLineId++;
+            }
+        }
+        return map;
+    }
+    function horizontalLineMap() {
+        let map = new Map();
+        // Horizontal Lines are assigned Ids starting from numberOfVerticalLines.
+        let currentLineId = numberOfVerticalLines;
+        for (let squareId = 0; squareId < totalSquares; squareId++) {
+            if (isStartOfHorizontalLine(squareId)) {
+                let first = squareId + (0 * squaresPerCol);
+                let second = squareId + (1 * squaresPerCol);
+                let third = squareId + (2 * squaresPerCol);
+                let fourth = squareId + (3 * squaresPerCol);
+                let squaresInLine = [first, second, third, fourth];
+                map.set(currentLineId, squaresInLine);
+                currentLineId++;
+            }
+        }
+        return map;
+    }
+    function upslashLineMap() {
+        let map = new Map();
+        // Upslash Lines are assigned Ids starting from numberOfVerticalLines + numberOfHorizontalLines.
+        let currentLineId = numberOfVerticalLines + numberOfHorizontalLines;
+        for (let squareId = 0; squareId < totalSquares; squareId++) {
+            if (isStartOfUpslashLine(squareId)) {
+                let first = squareId + 0 * (squaresPerCol + 1);
+                let second = squareId + 1 * (squaresPerCol + 1);
+                let third = squareId + 2 * (squaresPerCol + 1);
+                let fourth = squareId + 3 * (squaresPerCol + 1);
+                let squaresInLine = [first, second, third, fourth];
+                map.set(currentLineId, squaresInLine);
+                currentLineId++;
+            }
+        }
+        return map;
+    }
+    function downslashLineMap() {
+        let map = new Map();
+        // Downslash Lines are assigned Ids starting from numberOfVerticalLines + numberOfHorizontalLines + numberOfUpslashLines.
+        let currentLineId = numberOfVerticalLines + numberOfHorizontalLines + numberOfUpslashLines;
+        for (let squareId = 0; squareId < totalSquares; squareId++) {
+            if (isStartOfDownslashLine(squareId)) {
+                let first = squareId + 0 * (squaresPerCol - 1);
+                let second = squareId + 1 * (squaresPerCol - 1);
+                let third = squareId + 2 * (squaresPerCol - 1);
+                let fourth = squareId + 3 * (squaresPerCol - 1);
+                let squaresInLine = [first, second, third, fourth];
+                map.set(currentLineId, squaresInLine);
+                currentLineId++;
+            }
+        }
+        return map;
+    }
+    return completeMap;
+}
+
