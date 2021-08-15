00baeb83b97c363463cc3d42e2f1c5af59972044
nolastemgarden <nolastemgarden@gmail.com>
2021-08-15 15:47:27 -0500

    Update SettingsStepper.js

--- src/navigation/SettingsStepper.js
+++ src/navigation/SettingsStepper.js
@@ -337,4 +337,202 @@ export function SettingsStepper(props) {
         )
 
 
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // Navigation Buttons
+        ///////////////////////////////////////////////////////////////////////////////////////    
+        function BackButton(props) {
+            return (
+                <Button variant="text"
+                    onClick={goBackOneStep}
+                    sx={{ color: 'primary.main', mt: 1, mr: 1, fontSize: 'large' }}
+                >
+                    <ArrowBackIosIcon fontSize='small' />&ensp;Back
+                </Button>
+            )
+        }
+        function NextButton(props) {
+            return (
+                <Button variant="text"
+                    onClick={goToNextStep}
+                    sx={{ mt: 1, mr: 1, fontSize: 'large' }}
+                    disabled={completedSteps < activeStep}
+                >
+                    Next&ensp;<ArrowForwardIosIcon fontSize='small' />
+                </Button>
+            )
+        }
+        function StartGameButton(props) {
+            return (
+                <Button variant="text"
+                    disabled={completedSteps < activeStep + 1}
+                    component={RouterLink}
+                    to={{
+                        pathname: '/play',
+                        state: {
+                            playMode: playMode,
+                            questionType: questionType,
+                        }
+                    }}
+                    sx={{ mt: 1, mr: 1, fontSize: 'large', lineHeight: 1 }}
+                >
+                    Start<br />Game &ensp;<ArrowForwardIosIcon fontSize='small' />
+                </Button>
+            )
+        }
+
+        function SettingsStepperButton(props) {
+            return (
+                <Button
+                    variant="contained"
+                    onClick={props.setStateFunction(props.setting)}
+                    sx={{ mt: 1, mr: 1 }}
+                >
+                    {props.startIcon}{props.label}
+                </Button>
+            )
+        }
+
+
+
+
+        function SelectPlayModeStep(props) {
+            return (
+                <Step index={0} key={"Select Human Or Bot"}>
+                    <StepLabel>
+                        <Typography variant="body1" >Select Human or Bot: </Typography>
+                        <Typography sx={{ fontWeight: 'bold' }} >{playMode.toUpperCase()}</Typography>
+                    </StepLabel>
+                    <StepContent>
+                        <Box sx={{ mb: 2 }} display='flex' flexDirection='column' >
+                            <SettingsStepperButton
+                                label="Play vs. Bot"
+                                startIcon={"<i class='fas fa-robot'></i>"}
+                                onClick={() => {
+                                    setPlayMode("bot")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="Play vs. Human"
+                                onClick={() => {
+                                    setPlayMode("human")
+                                    goToNextStep()
+                                }}
+                            />
+                            {/* <BackButton disabled /> */}
+                        </Box>
+                    </StepContent>
+                </Step>
+            )
+        }
+
+        function SelectBotDifficultyStep(props) {
+            return (
+                <Step index={1}
+                    key={"Select Bot Difficulty"}
+                    disabled={(playMode === "human")}
+                >
+                    <StepLabel>
+                        <Typography variant="body1" >Select Bot Difficulty: </Typography>
+                        <Typography sx={{ fontWeight: 'bold' }} >{botDifficulty.toUpperCase()}</Typography>
+                    </StepLabel>
+                    <StepContent>
+                        <Typography>
+
+                        </Typography>
+                        <Box sx={{ mb: 2 }} display='flex' flexDirection='column' >
+                            <SettingsStepperButton
+                                label="Easy"
+                                onClick={() => {
+                                    setBotDifficulty("Easy")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="Medium"
+                                onClick={() => {
+                                    setBotDifficulty("Medium")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="Hard"
+                                onClick={() => {
+                                    setBotDifficulty("Hard")
+                                    goToNextStep()
+                                }}
+                            />
+                            <BackButton />
+                        </Box>
+                    </StepContent>
+                </Step>
+            )
+        }
+
+
+        function SelectQuestionTypeStep(props) {
+            return (
+                <Step index={2} key={"Select Type of Math Problem"}>
+                    <StepLabel>
+                        <Typography variant="body1" >Select Type of Math Problem: </Typography>
+                        <Typography variant="body1" sx={{ fontWeight: 'bold' }} >{questionType.toUpperCase()}</Typography>
+                    </StepLabel>
+                    <StepContent>
+                        <Box sx={{ mb: 2 }} display='flex' flexDirection='column' >
+                            <SettingsStepperButton
+                                label="Multiplication"
+                                onClick={() => {
+                                    setQuestionType("Multiplication")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="Division"
+                                onClick={() => {
+                                    setQuestionType("Division")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="Exponents"
+                                onClick={() => {
+                                    setQuestionType("Exponents")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="Algebra"
+                                onClick={() => {
+                                    setQuestionType("Algebra")
+                                    goToNextStep()
+                                }}
+                            />
+                            <SettingsStepperButton
+                                label="None"
+                                onClick={() => {
+                                    setQuestionType("None")
+                                    goToNextStep()
+                                }}
+                            />
+                            <BackButton />
+
+                        </Box>
+                    </StepContent>
+                </Step>
+            )
+        }
+
+
+        function EnterPlayerNamesStep(props) { }
+
+        function SetTimeLimitStep(props) { }
+
+
+
+
+
+
+    }
+
+}
 
