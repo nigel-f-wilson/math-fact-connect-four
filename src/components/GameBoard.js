2af4ea7a3f7f1193a8add7e42d1c1e12f79e6492
nolastemgarden <nolastemgarden@gmail.com>
2021-08-15 18:33:30 -0500

    Comment out testing moveList, add real props.moveList

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -8,8 +8,8 @@ const squarePercentage = '14.287%'
 const chipSizeRelativeToSquare = '84%'
 
 export function GameBoard(props) {
-    // const moveList = props.moveList // An Array of integers ranging -1 thru 41 of indeterminate length
-    const moveList = [-1, 1,2,3,4,5,-1,-1,6,-1,8,9,-1,10,-1,11,-1,12] // Testing Only
+    let {moveList, handleColumnClick} = props 
+    // moveList = [-1, 1,2,3,4,5,-1,-1,6,-1,8,9,-1,10,-1,11,-1,12] // Testing Only
     
     let boardData = boardDataFromMoveList(moveList)
     
