07bc755bc40841a6f9e37a02816f32b22b04187e
nolastemgarden <nolastemgarden@gmail.com>
2021-10-11 10:03:18 -0500

    Add comment
    I don't like that getColumnData is called 7 times and each time it calls getBoardData rather than caching this result and reusing it, updating it automatically only when the moveList updates. useEffect could help?

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -33,6 +33,7 @@ export function getBoardData(moveList) {
     })
     return data
 }
+// const boardData = getBoardData(moveList)  I don't like that getColumnData is called 7 times and each time it calls getBoardData rather than caching this result and reusing it, updating it automatically only when the moveList updates. useEffect could help?
 export function getColumnData(columnIndex, moveList) {
     let boardData = getBoardData(moveList)
     return boardData.filter((claimStatus, cellId) => cellId % 7 === columnIndex)
