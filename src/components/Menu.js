99d7694823f9dbd5c3096ff189a355a0db6cb8ff
nolastemgarden <nolastemgarden@gmail.com>
2021-10-20 10:20:15 -0500

    Paste in MUI sample Code

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,114 @@
 import * as React from 'react';
 import { Popover, Typography, Button } from '@material-ui/core'
+import {
+    Box,
+    Avatar,
+    Menu,
+    MenuItem,
+    ListItemIcon,
+    Divider,
+    IconButton,
+    Tooltip } from '@material-ui/core'
+
+
+
+
+
 // Icons
 import ReplayIcon from "@material-ui/icons/Replay";
 import MenuIcon from '@material-ui/icons/Menu';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+import Settings from '@material-ui/icons/Settings';
+import Logout from '@material-ui/icons/Logout';
 
-export function Menu(props) {
-    let { handleNewGameClick } = props
+
+
+export function AccountMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <React.Fragment>
+            <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', zIndex: 9999 }}>
+                <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+                <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+                <Tooltip title="Account settings">
+                    <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+                        <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem>
+                    <Avatar /> Profile
+                </MenuItem>
+                <MenuItem>
+                    <Avatar /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                    <ListItemIcon>
+                        <PersonAdd fontSize="small" />
+                    </ListItemIcon>
+                    Add another account
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
+}
+
+export function InGameMenu(props) {
+    let { handleNewGameClick, handleUndoClick } = props
     
     const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -21,7 +124,7 @@ export function Menu(props) {
     const id = open ? 'simple-popover' : undefined;
 
     return (
-        <div>
+        <React.Fragment>
             <Button variant="text"
                 aria-describedby={id} 
                 onClick={openMenu}
@@ -54,7 +157,7 @@ export function Menu(props) {
             >
                 <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
             </Popover>
-        </div>
+        </React.Fragment>
     );
 }
 
