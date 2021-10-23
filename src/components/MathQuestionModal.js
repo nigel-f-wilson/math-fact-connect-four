44c944ab494e70ec5867427fd847e1c96b4c9442
nolastemgarden <nolastemgarden@gmail.com>
2021-10-23 16:10:33 -0500

    Update Modal layout

--- src/components/MathQuestionModal.js
+++ src/components/MathQuestionModal.js
@@ -13,64 +13,103 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 })
 
 export function MathQuestionModal(props) {
-    let { playMode, questionType, open, closeQuestionModal, inputType, boardAreaSideLength  } = props
+    let { modalState, boardAreaSideLength, handleAnswerSubmit, closeQuestionModal } = props
+    let { isOpen, activeCell, question } = modalState
+    let { topic, inputType, instruction, formatString, vars, missingVar, } = question
 
-    let input = null
-    switch (inputType) {
-        case 'textField':
-            console.log('This modal has a text field');
-            input = <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            break;
-        case 'compareButtons':
-            console.log('This modal has compare buttons < >  =.')
-            // input = <CompareButton onClick={closeQuestionModal}>Disagree</Button>
-            input = <Button onClick={closeQuestionModal}>Disagree</Button>
-            break;
-        default:
-            console.error(`MathQuestionModal called with invalid questionType`);
-    }
-    
-    function DialogHeader(props) {
-        return (
-            <Box>
-                <Typography 
-                    variant='h3'
-                    align='center' 
-                >
-                    Compare
-                </Typography>
-            </Box>
-        )
-    }
+    let input = (inputType === "textField") ? 
+        <TextField 
+            id="answer"
+            variant="outlined"
+            handleAnswerSubmit={handleAnswerSubmit}
+
+        /> : 
+        <CompareButtons 
+            handleAnswerSubmit={handleAnswerSubmit}
+        />
     
     
+    console.log(`board area side length: ${boardAreaSideLength}`);
     
     return (
         <Dialog keepMounted
             disableEscapeKeyDown
-            open={open}
-            onClose={closeQuestionModal}  // Callback fired when the component requests to be closed.
+            open={isOpen}
+            // onClose={closeQuestionModal}  // Callback fired when the component requests to be closed.
             onBackdropClick={() => {}}
-            aria-describedby="alert-dialog-slide-description"
-            // PaperComponent="RoundPaper"
+            aria-describedby="math-question-dialog"
             TransitionComponent={Transition}
+            fullWidth={true}
+            maxWidth={false}
             sx={{
-                height: boardAreaSideLength,
-                width: boardAreaSideLength,
+                height: '100vh',
+                width: '100%',
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
+
+            }}
+            BackdropProps={{
+                style: {
+                    // backgroundColor: 'red',
+                    height: '100vh',
+                    width: '100vw',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }
+            }}
+            PaperProps={{
+                style: {
+                    // backgroundColor: 'red',
+                    height: `${0.9 * boardAreaSideLength}px`,
+                    width: `${0.9 * boardAreaSideLength}px`,
+                    borderRadius: '50%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '3rem'
+                }
             }}
             
         >
-            <DialogHeader />
+            <DialogTitle id="Instructions"
+                sx={{ border: 'solid red 1px', height: '30%' }}
+            >
+                {/* {instruction} */}
+                <Typography
+                    variant='h6'
+                    align='center'
+                >
+                    {instruction}
+                </Typography>
+            </DialogTitle>
 
-            {/* <MathQuestion /> */}
-            {/* <AnswerInput /> */}
             
-            
-            <DialogActions>
+            <DialogContent id="Equation" dividers
+                sx={{ width: '100%', 
+                border: 'solid red 1px',
+                flex: '0 0 40%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+
+
+                }}
+            >
+                <Typography
+                    variant='h1'
+                    align='center'
+                >
+                    {formatString}
+                </Typography>
+            </DialogContent>
+
+
+            <DialogActions id="AnswerInput"
+                sx={{ border: 'solid red 1px', height: '30%' }}
+            >
                 
                 {input}
                 <Button onClick={closeQuestionModal}>Agree</Button>
