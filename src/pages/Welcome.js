72caef41dc259ad23786c1a592bdaf11fe89b14a
nolastemgarden <nolastemgarden@gmail.com>
2021-11-20 16:24:43 -0600

    Complete MathTopicSelector buttons

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -10,7 +10,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRobot, faUser, faPlus, faTimes, faDivide, faSuperscript } from '@fortawesome/free-solid-svg-icons'
 
 // MUI  components
-import { Box, Button, Typography, Container, Divider,  Stack } from '@material-ui/core';
+import { Box, Grid, Button, Typography, Container, Divider,  Stack } from '@material-ui/core';
+
 
 
 const RobotIcon = () => { return <FontAwesomeIcon icon={faRobot} /> }
@@ -24,13 +25,13 @@ const SuperscriptIcon = () => { return <FontAwesomeIcon icon={faSuperscript} /> 
 
 export default function WelcomePage() {
     const [opponent, setOpponent] = React.useState('human') // Select 'human' or 'bot' .
-    const [mathTopics, setMathTopics] = React.useState({
-        'combining': true,
-        'multiplying': true,
-        'fractions': false,
-        'exponents': false,
-        'algebra': false,
-    })
+    const [mathTopics, setMathTopics] = React.useState([    // Select at least one topic
+        'combining',
+        'multiplying',
+        // 'fractions',
+        // 'exponents',
+        // 'algebra',
+    ])
     // Step 2: answer input types. Select from 'text field', 'compare buttons',  
     // const [answerInputFormats, setAnswerInputFormats] = React.useState(['text field', 'compare buttons'])
     // step 3: set time limit (optional)
@@ -41,12 +42,22 @@ export default function WelcomePage() {
             setOpponent(selection)
         }
     }
+    function handleMathTopicClick(clickedTopic) {
+        if (mathTopics.includes(clickedTopic)) {
+            if (mathTopics.length > 1) {
+                setMathTopics(mathTopics.filter(value => value !== clickedTopic))
+            }
+        }
+        else {
+            setMathTopics(mathTopics.concat(clickedTopic))
+        }
+    }
     
     return (
         <Container maxWidth='sm' sx={{ 
             width: '100%', 
-            height: '100%',
-            p: '1rem', 
+            height: '100vh',
+            p: '0.5rem', 
             display: 'flex', 
             flexDirection: 'column', 
             alignItems: 'center',
@@ -58,21 +69,26 @@ export default function WelcomePage() {
             <Typography color="text.primary" variant="h2" gutterBottom align='center' >
                 Math Fact<br/>Connect Four
             </Typography>
-            <Typography color="text.primary" variant="body1" gutterBottom align='center' >
+            <Typography color="text.primary" variant="body1" gutterBottom align='justify' >
                 Two players (or teams) take turns dropping chips. 
                 Get four consecutive chips in any row, column, or diagonal to win!  
-                If you incorrectly answer the math question that pops-up and your turn will be skipped. 
+                If you answer the math question that pops-up incorrectly your turn will be skipped.
             </Typography>
             
             <OpponentSelector opponent={opponent} clickHandler={handleOpponentClick} />
             
-            
+            <MathTopicSelector mathTopics={mathTopics} clickHandler={handleMathTopicClick} />
+
             
             
             <Button variant="contained" 
                 component={RouterLink}
-                to='/settings'
-                sx={{ m: 1 }}
+                // to='/play/:opponent'
+                to='/play'
+                sx={{ 
+                    width: '100%',
+                    margin: '1rem',
+                }}
             >
                 Play Now!
             </Button>
@@ -92,45 +108,118 @@ export default function WelcomePage() {
 
 function OpponentSelector(props) {
     let width = useScreenWidth()
-    const buttonWidth = '47%'
-
     const { opponent, clickHandler } = props
-    
     return (
         <Box 
             sx={{
                 width: '100%',
-                padding: '1rem 0', 
+                padding: '1rem', 
                 display: 'flex',
                 justifyContent: 'space-around',
                 borderBottom: 'solid #333 1px'
             }}
         >
-            <Button
-                startIcon={<UserIcon />}
-                variant={'contained'}
-                onClick={() => clickHandler("human")}
-                color={opponent === "human" ? 'primary' : 'secondary'}
-                children={width <= 600 ? 'vs. Human' : 'Play vs. Human'}
-                sx={{
-                    width: buttonWidth,
-                }}
-            >
-            </Button>
-            <Button
-                startIcon={<RobotIcon />}
-                variant={'contained'}
-                onClick={() => clickHandler("bot")}
-                color={opponent === "bot" ? 'primary' : 'secondary'}
-                sx={{
-                    width: buttonWidth,
-                }}
-                children={width <= 600 ? 'vs. Bot' : 'Play vs. Bot'}
-            >
-                
-            </Button>
+            <Grid container spacing={2} >
+                <Grid item xs={12} sm={6} >
+                    <Button
+                        startIcon={<UserIcon />}
+                        variant={'contained'}
+                        onClick={() => clickHandler("human")}
+                        color={opponent === "human" ? 'primary' : 'secondary'}
+                        // children={width <= 600 ? 'vs. Human' : 'Play vs. Human'}
+                        children={'Play vs. Human'}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} >
+                    <Button
+                        startIcon={<RobotIcon />}
+                        variant={'contained'}
+                        onClick={() => clickHandler("bot")}
+                        color={opponent === "bot" ? 'primary' : 'secondary'}
+                        // children={width <= 600 ? 'vs. Bot' : 'Play vs. Bot'}
+                        children={'Play vs. Bot'}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Grid>
+
+            </Grid>
         </Box>
-        
+    )
+}
+
+function MathTopicSelector(props) {
+    const { mathTopics, clickHandler } = props
+
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                padding: '1rem',
+                display: 'flex',
+                justifyContent: 'space-around',
+                borderBottom: 'solid #333 1px'
+            }}
+        >
+            <Grid container spacing={2} >
+                <Grid item xs={12} sm={6} >
+                    <Button
+                        startIcon={<PlusIcon />}
+                        variant={'contained'}
+                        onClick={() => clickHandler("combining")}
+                        color={mathTopics.includes("combining") ? 'primary' : 'secondary'}
+                        children={'combining'}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} >
+                    <Button
+                        startIcon={<TimesIcon />}
+                        variant={'contained'}
+                        onClick={() => clickHandler("multiplying")}
+                        color={mathTopics.includes("multiplying") ? 'primary' : 'secondary'}
+                        children={'multiplying'}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Grid>
+                
+                <Grid item xs={12} sm={6} >
+                    <Button
+                        startIcon={<DivideIcon />}
+                        variant={'contained'}
+                        onClick={() => clickHandler("fractions")}
+                        color={mathTopics.includes("fractions") ? 'primary' : 'secondary'}
+                        children={'fractions'}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Grid>
+                
+                <Grid item xs={12} sm={6} >
+                    <Button
+                        startIcon={<SuperscriptIcon />}
+                        variant={'contained'}
+                        onClick={() => clickHandler("exponents")}
+                        color={mathTopics.includes("exponents") ? 'primary' : 'secondary'}
+                        children={'exponents'}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Grid>
+            </Grid>
+            
+        </Box>
+
 
     );
 }
