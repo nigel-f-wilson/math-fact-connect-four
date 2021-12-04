56c5ec792bb3ea70525ccb5b8700e2963f98bf08
nolastemgarden <nolastemgarden@gmail.com>
2021-12-04 15:55:15 -0600

    Add 5 instructions for 5 more question types.

--- src/logic/instructionsAndFeedbackStrings.js
+++ src/logic/instructionsAndFeedbackStrings.js
@@ -16,3 +16,26 @@ export const missingAddendInstructions = [
     "What's missing?",
     "How many more?"
 ]
+
+export const missingDifferenceInstructions = [
+    "How far apart?",
+    "What's the Difference?",
+    "How much is left?"
+]
+export const missingMinuendInstructions = [
+    "Subtract",
+    "Minus what?",
+    "Take away how many?"
+]
+export const howFarApart = [
+    "How far apart?"
+]
+
+export const missingProductInstructions = [
+    "Multiply",
+    "What's the Product?",
+]
+export const missingFactorInstructions = [
+    "Find the missing factor",
+    "Times what?"
+]
