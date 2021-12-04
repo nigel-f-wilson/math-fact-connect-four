6d7415e9b6ca1d68e7e438c2a4a8ad5b2ce87e3e
nolastemgarden <nolastemgarden@gmail.com>
2021-12-04 16:38:33 -0600

    Extract and import questionTypes
    These are functions that take a difficulty string "easy" "medium" or "hard"

--- src/logic/questionGenerator.js
+++ src/logic/questionGenerator.js
@@ -8,6 +8,11 @@ import {
     missingProductInstructions,
     missingFactorInstructions 
 } from "./instructionsAndFeedbackStrings";
+    missingSumTwo,
+    missingSumThree,
+    missingAddendTwo,
+    missingAddendThree
+} from "./questionTypes";
 
 export function testQuestion() {
     let vars = [1, 2, 3, 6]
@@ -119,53 +124,6 @@ function getCombiningQuestion(difficulty) {
         let d = a + b + c
         return [a, b, c, d]
     }
-    
-    function missingSumTwo(difficulty) {
-        let vars = getSumOfTwoFact(difficulty)
-        return question = {
-            type: "missingSumTwo",
-            vars: vars,
-            correctAnswer: vars[2],
-            equationString: `${vars[0]} + ${vars[1]} = __`,
-            instructions: chooseRandomFromArray(missingSumInstructions),
-            inputType: "textField",
-        }
-    }
-    function missingSumThree(difficulty) {
-        let vars = getSumOfThreeFact(difficulty)
-        return question = {
-            type: "missingSumThree",
-            vars: vars,
-            correctAnswer: vars[3],
-            equationString: `${vars[0]} + ${vars[1]} + ${vars[2]} = __`,
-            instructions: chooseRandomFromArray(missingSumInstructions),
-            inputType: "textField",
-        }
-    }
-
-    function missingAddendTwo(difficulty) {
-        let vars = getSumOfTwoFact(difficulty)
-        return question = {
-            type: "missingAddendTwo",
-            vars: vars,
-            correctAnswer: vars[1],
-            equationString: `${vars[0]} + __ = ${vars[2]}`,
-            instructions: chooseRandomFromArray(missingAddendInstructions),
-            inputType: "textField",
-        }
-    }
-    function missingAddendThree(difficulty) {
-        let vars = getSumOfThreeFact(difficulty)
-        return question = {
-            type: "missingAddendThree",
-            vars: vars,
-            correctAnswer: vars[1],
-            equationString: `${vars[0]} + __ + ${vars[2]} = ${vars[3]}`,
-            instructions: chooseRandomFromArray(missingAddendInstructions),
-            inputType: "textField",
-        }
-    }
-    
     
 }
 
