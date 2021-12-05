f09b941e7df2d37c46e13d427d4279f6a56ec3d5
nolastemgarden <nolastemgarden@gmail.com>
2021-12-05 17:15:30 -0600

    Update combiningGenerator.js

--- src/logic/questionGenerators/combiningGenerator.js
+++ src/logic/questionGenerators/combiningGenerator.js
@@ -1,26 +1,30 @@
 // Exports helpers for when questionGenerator has selected the "combining" topic
 
 import { randomInt, chooseRandomFromArray } from "../lowLevelHelpers";
-import { 
-    missingSumInstructions,
-    missingAddendInstructions,
-    missingDifferenceInstructions,
-    missingMinuendInstructions,
-    missingProductInstructions,
-    missingFactorInstructions,
 
-} from "./instructionsAndFeedbackStrings";
+
+
+
+
+const missingMinuendInstructions = [
+    "Minus how many?",
+    "Take away what?",
+    "How far apart?",
+]
+const howFarApart = [
+    "How far apart?"
+]
 
 
 // Takes a difficulty param: easy medium or hard
 
 // some types only occur in hard mode such as 3 part sums and products
 export function getCombiningQuestion(difficulty) {
+    console.assert((difficulty === "easy" || difficulty === "medium" || difficulty === "hard"), `getCombiningQuestion recieved invalid difficulty ${difficulty}` )
+    console.log(`Generating Combining Question of difficulty ${difficulty}`)
     let combiningQuestionTypes = [
-        missingSumTwo,
-        missingSumThree,
-        missingAddendTwo,
-        missingAddendThree,
+        missingSum,
+        missingAddend,
         // "combineAndCompare",   // a + b _ c - d
         // "missingDifference",  // a - b = _
         // "missingMinuend",     // a - _ = c
@@ -31,33 +35,57 @@ export function getCombiningQuestion(difficulty) {
 }
 
 
-export function getSumOfTwoFact(difficulty) {
+function missingSum(difficulty) {
     const instructions = [
         "Add",
         "What's the Sum?",
         "Find the Total"
     ]
+
     if (difficulty === "easy") {
-        let a = randomInt(2, 60)
-        let b = randomInt(2, 25)
+        let a = randomInt(5, 75)
+        let b = randomInt(5, 25)
         let c = a + b
-        return [a, b, c]
+        let vars = [a, b, c]
+        return {
+            type: "missingSumTwo",
+            vars: vars,
+            correctAnswer: vars[2],
+            equationString: `${vars[0]} + ${vars[1]} = __`,
+            instructions: chooseRandomFromArray(instructions),
+            inputType: "textField",
+        }
     }
     else if (difficulty === "medium") {
-        let a = randomInt(60, 300)
-        let b = randomInt(6, 120)
+        let a = randomInt(20, 100)
+        let b = randomInt(20, 100)
         let c = a + b
-        return [a, b, c]
+        let vars = [a, b, c]
+        return {
+            type: "missingSumTwo",
+            vars: vars,
+            correctAnswer: vars[2],
+            equationString: `${vars[0]} + ${vars[1]} = __`,
+            instructions: chooseRandomFromArray(instructions),
+            inputType: "textField",
+        }
     }
     else if (difficulty === "hard") {
-        let a = randomInt(50, 900)
-        let b = randomInt(50, 300)
-        let c = a + b
-        return [a, b, c]
+        let a = randomInt(50, 200)
+        let b = randomInt(1, 100)
+        let c = randomInt(10, 100)
+        let d = a + b + c
+        let vars = [a, b, c, d]
+        return {
+            type: "missingSumThree",
+            vars: vars,
+            correctAnswer: vars[3],
+            equationString: `${vars[0]} + ${vars[1]} + ${vars[2]} = __`,
+            instructions: chooseRandomFromArray(instructions),
+            inputType: "textField",
+        }
     }
-    else { console.error(`getSumOfTwoFact called with Invalid difficulty: "${difficulty}".`) }
 }
-export function getSumOfThreeFact(difficulty) {
     const instructions = [
         "Subtract",
         "How far apart?",
@@ -72,11 +100,11 @@ export function getSumOfThreeFact(difficulty) {
         return [a, b, c, d]
     }
     else if (difficulty === "medium") {
-        let a = randomInt(12, 99)
-        let b = randomInt(12, 33)
-        let c = randomInt(12, 66)
-        let d = a + b + c
-        return [a, b, c, d]
+        vars.a = randomInt(100, 300)
+        vars.b = randomInt(5, 100)
+        vars.c = vars.a - vars.b
+        equationString = `${vars.a} - ${vars.b} = __`
+        correctAnswer = vars.c
     }
     else if (difficulty === "hard") {
         let a = randomInt(60, 600)
@@ -85,55 +113,65 @@ export function getSumOfThreeFact(difficulty) {
         let d = a + b + c
         return [a, b, c, d]
     }
-    else { console.error(`getSumOfThreeFact called with Invalid difficulty: "${difficulty}".`) }
-}
-
-export function missingSumTwo(difficulty) {
-    let vars = getSumOfTwoFact(difficulty)
     return {
-        type: "missingSumTwo",
+        type: "missingDifference",
         vars: vars,
-        correctAnswer: vars[2],
-        equationString: `${vars[0]} + ${vars[1]} = __`,
-        instructions: chooseRandomFromArray(missingSumInstructions),
-        inputType: "textField",
-    }
-}
-export function missingSumThree(difficulty) {
-    let vars = getSumOfThreeFact(difficulty)
-    return {
-        type: "missingSumThree",
-        vars: vars,
-        correctAnswer: vars[3],
-        equationString: `${vars[0]} + ${vars[1]} + ${vars[2]} = __`,
-        instructions: chooseRandomFromArray(missingSumInstructions),
+        equationString: equationString,
+        correctAnswer: correctAnswer,
+        instructions: chooseRandomFromArray(instructions),
         inputType: "textField",
     }
 }
 
-export function missingAddendTwo(difficulty) {
-    let vars = getSumOfTwoFact(difficulty)
-    return {
-        type: "missingAddendTwo",
-        vars: vars,
-        correctAnswer: vars[1],
-        equationString: `${vars[0]} + __ = ${vars[2]}`,
-        instructions: chooseRandomFromArray(missingAddendInstructions),
-        inputType: "textField",
+function missingAddend(difficulty) {
     const instructions = [
         "What's missing?",
         "How many more?"
     ]
+        
+    if (difficulty === "easy") {
+        let a = randomInt(1, 75)
+        let b = randomInt(1, 25)
+        let c = a + b
+        let vars = [a, b, c]
+        return {
+            type: "missingAddendTwo",
+            vars: vars,
+            correctAnswer: vars[1],
+            equationString: `${vars[0]} + __ = ${vars[2]}`,
+            instructions: chooseRandomFromArray(instructions),
+            inputType: "textField",
+        }
+    }
+    else if (difficulty === "medium") {
+        let a = randomInt(20, 100)
+        let b = randomInt(20, 100)
+        let c = a + b
+        let vars = [a, b, c]
+        return {
+            type: "missingAddendTwo",
+            vars: vars,
+            correctAnswer: vars[1],
+            equationString: `${vars[0]} + __ = ${vars[2]}`,
+            instructions: chooseRandomFromArray(instructions),
+            inputType: "textField",
+        }
+    }
+    else if (difficulty === "hard") {
+        let a = randomInt(50, 100)
+        let b = randomInt(1, 100)
+        let c = randomInt(1, 50)
+        let d = a + b + c
+        let vars = [a, b, c, d]
+        return {
+            type: "missingAddendThree",
+            vars: vars,
+            correctAnswer: vars[1],
+            equationString: `${vars[0]} + __ + ${vars[2]} = ${vars[3]}`,
+            instructions: chooseRandomFromArray(instructions),
+            inputType: "textField",
+        }
     }
 }
-export function missingAddendThree(difficulty) {
-    let vars = getSumOfThreeFact(difficulty)
-    return {
-        type: "missingAddendThree",
-        vars: vars,
-        correctAnswer: vars[1],
-        equationString: `${vars[0]} + __ + ${vars[2]} = ${vars[3]}`,
-        instructions: chooseRandomFromArray(missingAddendInstructions),
-        inputType: "textField",
-    }
-}
+
+
