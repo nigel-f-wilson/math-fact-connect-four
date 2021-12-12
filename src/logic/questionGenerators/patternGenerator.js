a83500fb2f53e2c204da3adfef54bbcfc0755cc3
nolastemgarden <nolastemgarden@gmail.com>
2021-12-12 05:04:53 -0600

    Create patternGenerator.js

---
+++ src/logic/questionGenerators/patternGenerator.js
@@ -0,0 +1,24 @@
+
+
+
+// This file will be used for building missing item in sequence questions
+
+
+
+
+// Instructions: "What's missing?"
+
+
+// EquationString:  "a, b, c, d"  With random element replaced as blank __
+
+// Adding Example: "3, 7, __, 15"
+
+// Easy mode will always make 0 the zeroth element in adding problems.
+// Easy mode will always make 1 the zeroth element in multiplying problems.
+
+
+// Medium mode will add a consistent amount but not start from 0 in adding problems.
+// Medium mode will multiply by a consistent step factor but not start from 1 in multiplying problems.
+
+
+// Hard mode will include Squares, Cubes, Triangle Numbers and Fibonacci Numbers.
