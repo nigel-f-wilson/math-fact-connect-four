659ab365e78f5e24e8b227dc2cebc4d11be4e4d4
nolastemgarden <nolastemgarden@gmail.com>
2021-12-12 09:37:25 -0600

    Add beginnings of a weighted Choice
    Want to take an array of tuples [percent chance of happening, function to call]

--- src/logic/lowLevelHelpers.js
+++ src/logic/lowLevelHelpers.js
@@ -13,5 +13,21 @@ export function randomInt(min, max) { //The maximum is exclusive and the minimum
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-// ADD a method to pick "randomly" from weighted distributions/
-// based on an array of tuples.
+
+
+export function weightedChoice(choicePercents, arrayToChooseFrom) {
+    let randomIndex = randomInt(0, arrayToChooseFrom.length)
+    let choice = arrayToChooseFrom[randomIndex]
+    // console.log(`CHOICE: ${choice}`);
+    return choice
+}
+
+// This function could be made more efficient through using sorted arrays and pointers that 
+// enabled us to not re-scan the leading portion of setTwo when we know we are looking for a 
+// higher number it only makes sense to look in higher indices. Besides that, early returns 
+// could be added in case the lowest or highest numbers in setOne fall outside the range of 
+// setTwo. This intersect is being made with setOne.length === 4 (cells in line) and a 
+// potentially much longer setTwo (player's numbers)
+export function intersect(setOne, setTwo) {
+    return setOne.filter(item => setTwo.includes(item))
+}
